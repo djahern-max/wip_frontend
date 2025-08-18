9dfa071b93f025ed3a605f18f684732661f7f4d6
djahern-max <danielaherniv@gmail.com>
2025-08-18 16:04:42 -0400

    added dynamic URL

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 // ===== frontend/src/services/api.js =====
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:8000/api/v1';
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:8000/api/v1';
 
 const api = axios.create({
     baseURL: API_BASE_URL,
